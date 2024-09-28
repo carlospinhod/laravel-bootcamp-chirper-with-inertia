8d7cf66fe69c418b4321f2efeb162892802effa0
Carlos Pinho <72576083+carlospinhod@users.noreply.github.com>
2024-09-28 14:05:42 +0100

    Editing Chirps

--- resources/js/Components/Chirp.vue
+++ resources/js/Components/Chirp.vue
@@ -46,10 +46,14 @@ const editing = ref(false);
                 </div>
                 <Dropdown v-if="chirp.user.id === $page.props.auth.user.id">
                     <template #trigger>
-                        <button
-                            class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 transition duration-150 ease-in-out"
-                            @click="editing = true"
-                        >
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                            </svg>
+                        </button>
+                    </template>
+                    <template #content>
+                        <button class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" @click="editing = true">
                             Edit
                         </button>
                     </template>
